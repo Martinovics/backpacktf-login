8d7c174ff3dad0295006d0ffcdf6f7e6ab4b52ba
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-09-03 17:49:18 +0200

    Update utils.py
    Added a basic check.
    Added return as string (as_strung) option.

--- tools/utils.py
+++ tools/utils.py
@@ -10,9 +10,16 @@ def generate_publickey(rsa_modulus, rsa_exponent):
 
 
 
-def encode_password(password, publickey = None, rsa_modulus = None, rsa_exponent = None):
+def encode_password(password, publickey=None, rsa_modulus=None, rsa_exponent=None, as_string=False):
     
     if publickey is not None:
-        return base64.b64encode(rsa.encrypt(password.encode('UTF-8'), publickey))
+        pwd = base64.b64encode(rsa.encrypt(password.encode('UTF-8'), publickey))
+    else:
+        if rsa_modulus is not None and rsa_exponent is not None:
+            pwd = base64.b64encode(rsa.encrypt(password.encode('UTF-8'), generate_publickey(rsa_modulus, rsa_exponent)))
+        else:
+            raise SyntaxError("You must set publickey or rsa_modulus and rsa_exponent.")
 
-    return base64.b64encode(rsa.encrypt(password.encode('UTF-8'), generate_publickey(rsa_modulus, rsa_exponent)))
+    if as_string:
+        return pwd.decode("utf-8")
+    return pwd
