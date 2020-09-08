0a0e8f1f478a0772e64a23cb42521477c0e36b39
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-09-08 20:34:40 +0200

    Update loginAsync.py

--- loginAsync.py
+++ loginAsync.py
@@ -15,8 +15,8 @@ from tools.config import Config as cfg
 
 class Login:
     
-    def __init__(self):
-        self.session = aiohttp.ClientSession(headers={'User-Agent': const.USER_AGENT})
+    def __init__(self, session):
+        self.session = session
         self.logged_in_to_steam = False
         self.logged_in_to_backpack = False
 
@@ -86,7 +86,92 @@ class Login:
         self.logged_in_to_steam = True
         print('Successfully logged in to steam.')
 
+
+
+
+    async def backpack_login(self) -> None:
+        if self.logged_in_to_backpack:
+            print("You are already logged in to backpack.tf.")
+            return
+
+        
+        resp = await self.session.post('https://backpack.tf/login')
+        if resp.status != 200:
+            raise Exception(f"There was an error while logging into backpack.tf.\n   Reason: {resp.status}")
+        
+        req_url = resp.url
+        resp = await resp.read()
+
+        soup = BeautifulSoup(resp.decode(encoding='utf-8', errors='ignore'), "lxml")
+        payload = {
+            'action': soup.findAll("input", {"name": "action"})[0]['value'],
+            'openidmode': soup.findAll("input", {"name": "openid.mode"})[0]['value'],
+            'openidparams': soup.findAll("input", {"name": "openidparams"})[0]['value'],
+            'nonce': soup.findAll("input", {"name": "nonce"})[0]['value']
+            }
+
+        resp = await self.session.post(req_url, data=payload)
+        if resp.status != 200:
+            raise Exception(f"There was an error while logging into backpack.tf.\n   Reason: {resp.status}")
+
+        self.logged_in_to_backpack = True
+        print('Successfully logged in to backpack.tf.')
+
+
+
+
+    async def login(self) -> None:
+        await self.steam_login()
+        await self.backpack_login()
+        print('Successfully logged in.')
+
+
+
+
+    async def steam_logout(self) -> None:
+        if not self.logged_in_to_steam:
+            print("You aren't logged in to steam.")
+            return
+
+
+        cookies = utils.jar_to_dict(self.session.cookie_jar)
+        resp = await self.session.post('https://steamcommunity.com/login/logout/', data={'sessionid':cookies['sessionid']})
+        
+        if resp.status != 200:
+            resp = await resp.read()
+            raise Exception(f"There was an error while logging out from steam.\n   Reason: {resp}")
+
+        self.logged_in_to_steam = False
+        print('Successfully logged out from steam.')
+    
+
+
+
+    async def backpack_logout(self) -> None:
+        if not self.logged_in_to_backpack:
+            print("You aren't logged in to backpack.tf.")
+            return
+
+
+        cookies = utils.jar_to_dict(self.session.cookie_jar)
+        resp = await self.session.get(f"https://backpack.tf/logout?user-id={cookies['user-id']}")
+
+        if resp.status != 200:
+            resp = await resp.read()
+            raise Exception(f"There was an error while logging out from backpack.tf.\n   Reason: {resp}")
+        
+        self.logged_in_to_backpack = False
+        print('Successfully logged out from backpack.tf.')
+
+
+
+
+    async def logout(self) -> None:
+        await self.steam_logout()
+        await self.backpack_logout()
+        
         await self.session.close()
+        print('Successfully logged out.')
 
 
 
@@ -94,8 +179,14 @@ class Login:
 
 
 async def main():
-    loginSession = Login()
-    await loginSession.steam_login()
+
+    async with aiohttp.ClientSession(headers={'User-Agent': const.USER_AGENT}) as session:
+
+        loginSession = Login(session=session)
+
+        await loginSession.login()
+        await asyncio.sleep(5)
+        await loginSession.logout()
 
 
 
