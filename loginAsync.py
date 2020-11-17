c84fc62471a4caf02234984df9efc00f23e669df
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-11-17 21:53:48 +0100

    Update loginAsync.py

--- loginAsync.py
+++ loginAsync.py
@@ -18,8 +18,6 @@ class Login:
     
     def __init__(self, session):
         self.session = session
-        self.logged_in_to_steam = False
-        self.logged_in_to_backpack = False
 
 
 
@@ -31,10 +29,6 @@ class Login:
 
 
     async def steam_login(self) -> None:
-        if self.logged_in_to_steam:
-            print("You are already logged in to steam.")
-            return
-
 
         resp = await self.session.post('https://steamcommunity.com/login/getrsakey/', data={'username': cfg.USERNAME})
         ourRsa = await resp.json()
@@ -67,7 +61,7 @@ class Login:
             'donotcache': str(int(time.time() * 1000))
             }
 
-        resp = await self.session.post("https://store.steampowered.com/login/dologin", params=payload)
+        resp = await self.session.post("https://store.steampowered.com/login/dologin", data=payload)
         resp = await resp.json()
 
         if not resp['success']:
@@ -80,14 +74,15 @@ class Login:
             await self.session.post(url, data=resp['transfer_parameters'])
 
 
+        '''
         stm_cookies = utils.jar_to_dict(self.session.cookie_jar)
-
         cookies = SimpleCookie(
             f"sessionid={stm_cookies['sessionid']}; Domain=steamcommunity.com; "
             f"sessionid={stm_cookies['sessionid']}; Domain=store.steampowered.com; "
             )
         
         self.session.cookie_jar.update_cookies(cookies)
+        '''
 
 
         # check whether we are really logged in
@@ -99,7 +94,6 @@ class Login:
         steamID64 = re.findall(r'g_steamID = "(.*?)";', resp)
 
         if username and steamID64:
-            self.logged_in_to_steam = True
             print(f'Successfully logged in to steam as {username[-1]} ({steamID64[-1]}).')
         else:
             raise Exception("There was an error while logging into steam.\n   Reason: unknown")
@@ -108,11 +102,7 @@ class Login:
 
 
     async def backpack_login(self) -> None:
-        if self.logged_in_to_backpack:
-            print("You are already logged in to backpack.tf.")
-            return
 
-        
         resp = await self.session.post('https://backpack.tf/login')
         if resp.status != 200:
             raise Exception(f"There was an error while logging into backpack.tf.\n   Reason: {resp.status}")
@@ -142,8 +132,6 @@ class Login:
 
         if login_data:
             steamID64, username = login_data[0]
-
-            self.logged_in_to_backpack = True
             print(f"Successfully logged in to backpack.tf as {username} ({steamID64}).")
         else:
             raise Exception("There was an error while logging into backpack.tf.\n   Reason: unknown")
@@ -160,10 +148,6 @@ class Login:
 
 
     async def steam_logout(self) -> None:
-        if not self.logged_in_to_steam:
-            print("You aren't logged in to steam.")
-            return
-
 
         cookies = utils.jar_to_dict(self.session.cookie_jar)
         resp = await self.session.post('https://steamcommunity.com/login/logout/', data={'sessionid':cookies['sessionid']})
@@ -172,17 +156,12 @@ class Login:
             resp = await resp.read()
             raise Exception(f"There was an error while logging out from steam.\n   Reason: {resp}")
 
-        self.logged_in_to_steam = False
         print('Successfully logged out from steam.')
-    
+
 
 
 
     async def backpack_logout(self) -> None:
-        if not self.logged_in_to_backpack:
-            print("You aren't logged in to backpack.tf.")
-            return
-
 
         cookies = utils.jar_to_dict(self.session.cookie_jar)
         resp = await self.session.get(f"https://backpack.tf/logout?user-id={cookies['user-id']}")
@@ -190,8 +169,7 @@ class Login:
         if resp.status != 200:
             resp = await resp.read()
             raise Exception(f"There was an error while logging out from backpack.tf.\n   Reason: {resp}")
-        
-        self.logged_in_to_backpack = False
+
         print('Successfully logged out from backpack.tf.')
 
 
